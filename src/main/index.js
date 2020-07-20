8379a3593f031c8608a02d2556a8cbde5da8d313
Kamesuta <kamesuta@gmail.com>
2020-07-20 13:48:08 +0900

    diff merge

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,6 @@
 const {ipcMain} = require('electron')
 const fs = require("fs")
+const path = require("path")
 const git = require('isomorphic-git')
 const http = require('isomorphic-git/http/node')
 
@@ -56,57 +57,94 @@ ipcMain.handle('git.pull', async (event, data) => {
 })
 
 ipcMain.handle('git.diff', async (event, data) => {
+  const dir = data.local
   const commitA = await git.log({
     fs,
-    dir: data.local,
+    dir,
     depth: 1,
     ref: 'master'
   })
   const commitB = await git.log({
     fs,
-    dir: data.local,
+    dir,
     depth: 1,
     ref: 'origin/master'
   })
   return git.walk({
     fs,
-    dir: data.local,
-    trees: [git.TREE({ ref: commitA[0].oid }), git.TREE({ ref: commitB[0].oid })],
-    map: async function(filepath, [A, B]) {
+    dir,
+    trees: [git.TREE({ ref: commitA[0].oid }), git.TREE({ ref: commitB[0].oid }), git.WORKDIR()],
+    map: async function(filepath, [A, B, C]) {
       // ignore directories
-      if (filepath === '.') {
+      if (filepath === '.' || filepath.startsWith('.git/')) {
         return
       }
-      if (A !== null && (await A.type()) === 'tree' || B !== null && (await B.type()) === 'tree') {
+
+      /*
+      if ((A !== null && (await A.type()) === 'tree')
+        || (B !== null && (await B.type()) === 'tree')
+        || (C !== null && (await C.type()) === 'tree')) {
         return
       }
+       */
 
       // generate ids
       const Aoid = A === null ? undefined : await A.oid()
       const Boid = B === null ? undefined : await B.oid()
+      const Coid = C === null ? undefined : await C.oid()
 
-      // determine modification type
-      let type = 'equal'
-      if (Aoid !== Boid) {
-        type = 'modify'
-      }
-      if (Aoid === undefined) {
-        type = 'add'
-      }
-      if (Boid === undefined) {
-        type = 'remove'
-      }
-      if (Aoid === undefined && Boid === undefined) {
-        console.log('Something weird happened:')
-        console.log(A)
-        console.log(B)
-      }
+      const fullpath = path.join(dir, filepath)
 
       return {
-        path: `/${filepath}`,
-        type: type,
+        A,
+        B,
+        C,
+        Aoid,
+        Boid,
+        Coid,
+        filepath,
+        fullpath,
       }
     }
+  }).then(async changes => {
+    return Promise.all(changes.map(async change => {
+      const {filepath, fullpath} = change
+      // determine modification type
+      if (change.Aoid === undefined && change.Boid === undefined) {
+        // unknown
+        console.log('Something weird happened:')
+        console.log(change.A)
+        console.log(change.B)
+        console.log(change.C)
+      } else if (change.Aoid === change.Boid) {
+        // equal
+      } else if (change.Boid === undefined) {
+        // remove
+        if (change.Coid !== undefined) {
+          await fs.promises.unlink(fullpath).catch()
+          await git.remove({
+            fs,
+            dir,
+            filepath,
+          })
+        }
+      } else {
+        // add, modify
+        if (change.Boid != change.Coid) {
+          await fs.promises.writeFile(fullpath, await change.B.content())
+          await git.add({
+            fs,
+            dir,
+            filepath,
+          })
+        }
+      }
+      return change
+    }))
+  }).then(async result => {
+    const commit = commitB[0].oid
+    await fs.promises.writeFile(path.join(dir, `/.git/refs/heads/master`), commit);
+    return result
   }).then(result => {
     console.log(result)
     return {
@@ -114,6 +152,7 @@ ipcMain.handle('git.diff', async (event, data) => {
       result: JSON.stringify(result),
     }
   }).catch(err => {
+    console.error(err)
     return {
       success: false,
       reason: err,
